ca858b0008e2ca649fb2a215f1477f0a9749fecf
Aleti Shivani <24311a66z1@aiml.sreenidhi.edu.in>
2026-07-06 14:19:11 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,48 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user_score = 0
+computer_score = 0
+
+print("===== ROCK PAPER SCISSORS GAME =====")
+
+while True:
+    user_choice = input("\nChoose Rock, Paper, or Scissors: ").lower()
+
+    if user_choice not in choices:
+        print("Invalid Choice! Try Again.")
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print("Your Choice:", user_choice)
+    print("Computer Choice:", computer_choice)
+
+    if user_choice == computer_choice:
+        print("It's a Tie!")
+
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors")
+        or (user_choice == "paper" and computer_choice == "rock")
+        or (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        print("You Win!")
+        user_score += 1
+
+    else:
+        print("Computer Wins!")
+        computer_score += 1
+
+    print("\nScore Board")
+    print("You:", user_score)
+    print("Computer:", computer_score)
+
+    play_again = input("\nPlay Again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        print("\nGame Over!")
+        print("Final Score")
+        print("You:", user_score)
+        print("Computer:", computer_score)
+        break
